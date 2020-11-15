02b54e30207a64b6efd8636aa4812828dd86938f
lenny <lennylee91@gmail.com>
2020-11-14 19:36:43 -0800

    added new and create functions, test users/new view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+    #GET /register - New User Action
+    def new 
+        render :plain => "Hello World"
+    end
+
+    #POST /users - Create the User Action
+    def create
+    end
+
 end
